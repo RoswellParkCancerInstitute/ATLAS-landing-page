8de4132d33ca5cd2c1cd04a2cca4c23aee7be096
Sridhar Mane <Sridhar.Mane@RoswellPark.org>
2016-05-05 18:14:16 -0400

    created landing page with buttons, logos and analytics

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,50 @@
  * Controller of the appinstallendpointApp
  */
 angular.module('appinstallendpointApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope,Ref, $firebaseArray) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.clicks = $firebaseArray(Ref.child('clicks'));
+    $scope.views = $firebaseArray(Ref.child('views'));
+    $scope.registrations = $firebaseArray(Ref.child('registrations'));
+
+    $scope.recordView = function(){
+      $scope.views.$add({
+        userAgent:navigator.userAgent,
+        timeStamp:new Date().getTime()
+      }).then(function(){
+        console.log('Saved View');
+      },function(error){
+        console.log('Save View Error', error);
+      });
+    };
+
+    $scope.recordClick = function(osType,appName){
+      $scope.clicks.$add({
+        osType:osType,
+        appName:appName,
+        timeStamp:new Date().getTime()
+      }).then(function(){
+        console.log('Saved Click');
+      },function(error){
+        console.log('Save Click Error', error);
+      });
+    };
+
+    $scope.registerEmail = function(appName){
+      $scope.registrations.$add({
+        appName:appName,
+        osType:$scope.userEmail,
+        timeStamp:new Date().getTime()
+      }).then(function(){
+        console.log('Saved Email');
+      },function(error){
+        console.log('Save Email Error', error);
+      });
+    };
+
   });
