be466a632005eeff92c9f368d30d818e4bed6ae6
Sridhar Mane <sridhars4s@gmail.com>
2017-05-11 14:07:11 -0400

    Added Hammerjs to project, a material dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
+import 'hammerjs';
 // import { BrowserAnimationsModule } from '@angular/animations/browser/';
 // import { Browser } from '@angular/animations';
 
